ec06eec6ff156dbf0ef070295d712317c8926350
pailwan <sachinpailwan@gmail.com>
2020-07-06 22:51:04 +0530

    'init'

---
+++ src/main/java/com/pailsom/SpringAopPerformanceLoggingApplication.java
@@ -0,0 +1,13 @@
+package com.pailsom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopPerformanceLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAopPerformanceLoggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pailsom/SpringAopPerformanceLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pailsom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopPerformanceLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
